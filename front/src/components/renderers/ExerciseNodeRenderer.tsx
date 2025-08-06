55111d1246c28257516a0b66ec10fbf0f5dfc843
faithleysath <faithleysath@gmail.com>
2025-08-06 14:44:20 +0800

    练习题题解

--- front/src/components/renderers/ExerciseNodeRenderer.tsx
+++ front/src/components/renderers/ExerciseNodeRenderer.tsx
@@ -3,6 +3,12 @@ import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import type { Node, ProcessedLightweightNode } from '../../lib/types';
 import { useMemo } from 'react';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
 
 interface ExerciseNodeRendererProps {
   node: ProcessedLightweightNode;
@@ -19,7 +25,7 @@ const ExerciseNodeRenderer = ({ fullNode }: ExerciseNodeRendererProps) => {
   }, [fullNode.solution]);
 
   return (
-    <div className="text-base p-4 border-l-4 border-blue-400 bg-blue-50 my-2">
+    <div className="text-base p-4 pb-0 border-l-4 border-blue-400 bg-blue-50 my-2">
       <MDEditor.Markdown
         source={displayContent}
         style={{ backgroundColor: 'transparent' }}
@@ -27,15 +33,21 @@ const ExerciseNodeRenderer = ({ fullNode }: ExerciseNodeRendererProps) => {
         rehypePlugins={[[rehypeKatex, { throwOnError: false }]]}
       />
       {solutionContent && (
-        <>
-          <hr className="my-4" />
-          <MDEditor.Markdown
-            source={solutionContent}
-            style={{ backgroundColor: 'transparent' }}
-            remarkPlugins={[remarkMath]}
-            rehypePlugins={[[rehypeKatex, { throwOnError: false }]]}
-          />
-        </>
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="item-1">
+            <AccordionTrigger className="text-blue-700 hover:no-underline hover:text-blue-800 cursor-pointer">
+              显示/隐藏题解
+            </AccordionTrigger>
+            <AccordionContent>
+              <MDEditor.Markdown
+                source={solutionContent}
+                style={{ backgroundColor: 'transparent' }}
+                remarkPlugins={[remarkMath]}
+                rehypePlugins={[[rehypeKatex, { throwOnError: false }]]}
+              />
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       )}
     </div>
   );
