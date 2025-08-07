be1d22b689474cf088ccae57e5796e20685bd3f3
faithleysath <faithleysath@gmail.com>
2025-08-07 09:35:30 +0800

    撤销修复拖动方向

--- src/App.tsx
+++ src/App.tsx
@@ -164,16 +164,16 @@ function App() {
   return (
     <div className="h-screen w-screen bg-background text-foreground">
       <ResizablePanelGroup direction="horizontal">
-        <ResizablePanel defaultSize={20} minSize={15} order={1}>
+        <ResizablePanel defaultSize={20} minSize={15}>
           <LeftSidebar />
         </ResizablePanel>
         <ResizableHandle withHandle />
-        <ResizablePanel defaultSize={25} minSize={15} order={3}>
-          <RightSidebar />
+        <ResizablePanel defaultSize={55} minSize={30} key={structureVersion}>
+          <MainContent />
         </ResizablePanel>
         <ResizableHandle withHandle />
-        <ResizablePanel defaultSize={55} minSize={30} key={structureVersion} order={2}>
-          <MainContent />
+        <ResizablePanel defaultSize={25} minSize={15}>
+          <RightSidebar />
         </ResizablePanel>
       </ResizablePanelGroup>
       <Toaster />
